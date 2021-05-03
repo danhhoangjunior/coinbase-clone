459acb569e004940b9e656ffafb9f4561d09bd78
arjun-dureja <arjun.dureja1000@gmail.com>
2021-05-02 23:48:32 -0400

    finish button

--- App.tsx
+++ App.tsx
@@ -1,13 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Animated,
+  ScrollView,
+} from 'react-native';
 
 export default function App() {
+  const animatedValue = new Animated.Value(1);
+
+  const handlePressIn = () => {
+    Animated.spring(animatedValue, {
+      toValue: 0.97,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const handlePressOut = () => {
+    Animated.spring(animatedValue, {
+      toValue: 1,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const animatedStyle = {
+    transform: [{ scale: animatedValue }],
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Animated.View style={[styles.btnContainer, animatedStyle]}>
+        <TouchableHighlight
+          onPressIn={handlePressIn}
+          onPressOut={handlePressOut}
+          style={{ borderRadius: 8 }}
+        >
+          <View style={styles.btn}>
+            <Text style={styles.btnText}>Add payment method</Text>
+          </View>
+        </TouchableHighlight>
+      </Animated.View>
+      <StatusBar style='auto' />
+    </ScrollView>
   );
 }
 
@@ -17,5 +54,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  btnContainer: {
+    width: '85%',
+    borderRadius: 8,
+  },
+  btn: {
+    width: '100%',
+    height: 55,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgb(0, 82, 248)',
+    borderRadius: 8,
+  },
+  btnText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
