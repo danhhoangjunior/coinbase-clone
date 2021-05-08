c4c5ba791d7c6e8d97664adb3f32d341a64cae42
arjun-dureja <arjun.dureja1000@gmail.com>
2021-05-08 17:50:54 -0400

    create newslist and newslistitem components

---
+++ src/components/NewsListItem.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const NewsListItem = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default NewsListItem;
